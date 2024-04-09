8b588f39d84d29e6f6fb583893a6338163221c4b
SagiSama <62695992+SagiSama@users.noreply.github.com>
2024-04-08 21:41:33 -0700

    C#

--- CSharp/ConsoleApp1/Day1/Assignment2/PracticeArrays.cs
+++ CSharp/ConsoleApp1/Day1/Assignment2/PracticeArrays.cs
@@ -6,5 +6,22 @@ public class PracticeArrays
     public static void CopyingArray()
     {
         Console.WriteLine("copy");
+        int[] origArr = new[] { 2, 3, 4, 1, 100, 3, 43, 1, 9, 0 };
+        int[] copyArr = new int[origArr.Length];
+
+        for (int i = 0; i < origArr.Length; i++)
+        {
+            copyArr[i] = origArr[i];
+        }
+        Console.Write("Original arr contents => ");
+        for (int i = 0; i < origArr.Length; i++)
+        {
+            Console.Write(origArr[i] +", ");
+        }
+        Console.Write("\nCopy arr contents     => ");
+        for (int i = 0; i < origArr.Length; i++)
+        {
+            Console.Write(copyArr[i] +", ");
+        }
     }
 }
--- CSharp/ConsoleApp1/Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ CSharp/ConsoleApp1/Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cfe5621cff663df70206090627ce8f57378bc02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4df5bbf5f7aecb5fce2e9c40f5846d1c6f816d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
